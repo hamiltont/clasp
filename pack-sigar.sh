d309690c52416d04f2170c040d6cd4a52d0558f3
Brandon Amos <bdamos@vt.edu>
2013-03-20 08:23:34 -0400

    Clasp: Add pack-sigar script to add '.so' libraries to the fat jar.

---
+++ pack-sigar.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Packs the '.so' files from Sigar into the assembly jar.
+
+function die { echo $@; exit 1; }
+
+JAR=target/Clasp-Assembly.jar
+[[ -e $JAR ]] || die "Unable to find '$JAR'"
+
+# TODO Where should these libraries be packed?
+SIGAR_LIB=lib/hyperic-sigar-1.6.4/sigar-bin/lib
+jar uf $JAR -C $SIGAR_LIB .
