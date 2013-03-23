3936b2863f2e751d57e65dea500562d5ab424465
Brandon Amos <bdamos@vt.edu>
2013-03-23 17:09:18 -0400

    Clasp: Add minimal 'aapt' wrapper to dump package info.

---
+++ src/core/sdktools/proxy_aapt.scala
@@ -0,0 +1,27 @@
+package core.sdktools
+
+import scala.sys.process.stringToProcess
+import scala.sys.process.Process
+
+import sdk_config.log.debug
+import sdk_config.log.error
+import sdk_config.log.info
+
+/**
+ * Provides an interface to the
+ * [[https://developer.android.com/tools/help/aapt.html aapt]]
+ * command line tool.
+ * 
+ * This, along with other components of the Android SDK, is included in
+ * [[core.sdktools.sdk]].
+ */
+trait AaptProxy {
+  val aapt:String = sdk_config.config.getString(sdk_config.aapt_config)
+  import sdk_config.log.{error, debug, info, trace}
+    
+  def aapt_dump(flag: String, apk: String): String = {
+    val command = s"$aapt dump $flag $apk"
+    val output: String = command !!;
+    output
+  }
+}
--- src/core/sdktools/sdk_facade.scala
+++ src/core/sdktools/sdk_facade.scala
@@ -12,15 +12,21 @@ import com.typesafe.config.ConfigFactory
 /**
  * The entire SDK tool facade!
  */
-object sdk extends AndroidProxy with EmulatorProxy with AdbProxy with TelnetProxy {}
+object sdk extends AndroidProxy 
+              with EmulatorProxy
+              with AdbProxy
+              with TelnetProxy
+              with AaptProxy {}
 
 object sdk_config {
   // TODO run checks to ensure that all three of these can be accessed
   val android_config  = "sdk.android"
   val emulator_config = "sdk.emulator"
   val adb_config      = "sdk.adb"
+  val aapt_config      = "sdk.aapt"
   val config: Config  = ConfigFactory.load()
-  // TODO make this return the proper class at runtime. Currently only returns core.sdk_config
+  // TODO make this return the proper class at runtime.
+  // Currently only returns core.sdk_config
   lazy val log = LoggerFactory.getLogger(getClass()) 
 }
 
