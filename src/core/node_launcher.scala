8165b87b41bbc19df2a41082d385ba0af4ebe079
Hamilton Turner <hamiltont@gmail.com>
2013-04-13 08:41:15 -0400

    Accepts --client flag, starts client/server differently. Uses scallop parsing library
    Does not actally perform a remote connection yet

--- src/core/node_launcher.scala
+++ src/core/node_launcher.scala
@@ -12,6 +12,9 @@ import core.sdktools.EmulatorOptions
 
 import akka.actor._
 
+// Used for command line parsing
+import org.rogach.scallop._
+
 /*
  * Handles starting nodes, either to represent this 
  * computer or other computers on the network. If this 
@@ -26,24 +29,64 @@ object Clasp extends App {
   import log.{error, debug, info, trace}
 
   val system = ActorSystem("clasp")
-  val launcher = system.actorOf(Props[NodeLauncher], name="nodelauncher")
+
+  val conf = new Conf(args)
+  if (conf.client()) {
+    info("I am a client!") 
+    // Create new node, which will auto-register with
+    // the NodeLauncher
+    var n = system.actorOf(Props[Node], name="10.0.2.6")
+    //n ! "rundefault"
+    info("Created Node")
+  }
+  else {
+    info("I am a server!")
+    // Create NodeLauncher, which will automatically 
+    // ssh into each worker computer and properly 
+    // run clasp as a client
+    val launcher = system.actorOf(Props[NodeLauncher], name="nodelauncher")
+    
+    // TODO ask configuration if we should create a local node object
+  }
+
+  info("sleeping")
+  Thread.sleep(20000)
+
+  info("Shutting down system")
+  system.shutdown
 }
 
+class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
+  version("Clasp 0.0.0")
+  banner("""Usage: clasp [-c|--client]
+    |By default clasp runs as though it was a server with only 
+    |the local node. This makes it easier for people running in
+    |a non-distributed manner. If you use sbt, then to run a 
+    |client use sbt "run --client". To run a whole system you
+    |need a server running on the main node and then clients on
+    |all other nodes
+    |""".stripMargin)
+
+  val client = opt[Boolean](descr = "Should this run as a client instance")
+}
+
+// todo call into ssh and start all clients. They can
+// then register back with the nodelauncher
 class NodeLauncher extends Actor {
   lazy val log = LoggerFactory.getLogger(getClass())
   import log.{error, debug, info, trace}
   val nodes = ListBuffer[ActorRef]()
 
-  var n = context.actorOf(Props[Node], name="local")
-  n ! "rundefault"
-  info("Created Node")
-  Thread.sleep(20000)
-  n ! "cleanup"
-  info("Cleaned  Node")
+  //var n = context.actorOf(Props[Node], name="10.0.2.6")
+  //n ! "rundefault"
+  //info("Created Node")
 
-
+  // TODO replace this with monitoring child lifecycle
   def receive = {
-    case "node_up" => nodes += sender
+    case "node_up" => { 
+      info("A node has registered!")
+      nodes += sender 
+    }
     case "node_down" => nodes -= sender
   }
 
@@ -59,7 +102,9 @@ class Node extends Actor {
   import core.sdktools.EmulatorOptions
  
   override def preStart() = {
-    context.parent ! "node_up"
+    // Lookup the NodeLauncher
+    val launcher = context.actorFor("akka.tcp://clasp@10.0.2.6:2552/user/nodelauncher")
+    launcher ! "node_up"
   }
 
   override def postRestart(reason: Throwable) = {
@@ -69,7 +114,11 @@ class Node extends Actor {
 
   override def postStop() = {
     // TODO crash emulators? No....
-    context.parent ! "node_down"
+    // TODO - should I even do this, or just watch for the Terminate message from my 
+    // children in the NodeLauncher
+    val launcher = context.actorSelection("akka.tcp://clasp@10.0.2.6:2552/nodelauncher")
+    launcher ! "node_down"
+    info("Node " + hostname + " has stopped");
   }
 
   val devices: MutableList[ActorRef] = MutableList[ActorRef]()
