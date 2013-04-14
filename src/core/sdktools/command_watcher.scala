c92d50771b6e14ff0e6ccf280949b7af583f55d6
George Burgess IV <georgegburgess@gmail.com>
2013-04-14 11:32:46 -0400

    Nearly all tests written for AndroidCommand, TimedCommandWatcher, etc. All written tests pass. The only (main) part that I've yet to test is AsynchronousCommand's notification of an actor on the completion of the future.

--- src/core/sdktools/command_watcher.scala
+++ src/core/sdktools/command_watcher.scala
@@ -1,103 +1,170 @@
-package core.sdktools
+package core
 
-import scala.actors._
-import scala.concurrent.ops._
-import scala.concurrent.duration._
-import scala.concurrent.Await
-import scala.concurrent.Future 
+import akka.actor._
+import akka.actor.ActorDSL._
+import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.locks.ReentrantLock
+import scala.actors.Futures.alarm
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.util.{Try, Success, Failure}
+import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.concurrent.ops._
 import scala.language.postfixOps
 
-class AndroidCommand[T](val function: (Unit => T)) {
+class AndroidCommand[T](val function: (() => T)) {
   require(function != null)
 
   private var _command_result: Option[T] = None
   private var _command_success: Option[Boolean] = None
   private var _attached_watchers: List[CommandWatcher] = List()
 
-  def command_result = _command_success.flatMap {
-    if (_)
+  def result =
+    if (_command_success.getOrElse(false))
       _command_result
-    else 
-      None 
-  }
+    else
+      None
 
-  def command_success = _command_success
+  def success = _command_success
 
   protected def command_check_succeeded(res: T): Boolean = true
 
-  def add_watcher(cw: CommandWatcher) {
-    _attached_watchers = cw :: _attached_watchers 
-  }
-
-  def rm_watcher(cw: CommandWatcher) {
-    // Filter instead of remove because there could be multiple
-    // instances of that Watcher in the list.
-    command_result.filter { _ != cw }
-  }
-
-  def run_command() {
-    val res = function()
-    _command_result = Some(res)
-    _command_success = Some(command_check_succeeded(res))
+  def run() { 
+    // If we've not tried to run the command already...
+    if (_command_success == None) {
+      val res = function()
+      _command_result = Some(res)
+      _command_success = Some(command_check_succeeded(res))
+    }
   }
 
   def force_fail(even_if_success: Boolean = false) {
-    val set_to = _command_success getOrElse false
-    if (!set_to || even_if_success) {
-      _command_success = Some(set_to)
+    val succeeded = _command_success getOrElse false
+    if (!succeeded || even_if_success) {
+      _command_success = Some(false)
     }
   }
 }
 
-class AsynchronousCommand[T](function: (Unit => T), val max_wait: Duration = Duration.Inf, val notify_targ: Option[Actor] = None)
-    extends AndroidCommand[T](function) with Actor {
-  require(duration != null && notify_targ != null)
+class AsynchronousCommand[T](function: (() => T), val max_wait: Duration = Duration.Inf, val notify_targ: Option[ActorRef] = None)
+  extends AndroidCommand[T](function) {
+  require(max_wait != null && notify_targ != null)
 
-  private var _cmd_future: Option[Future[Option[T]]] = None
+  private var _watchers = List[CommandWatcher]()
+  private val _started = new AtomicBoolean(false)
+  private val _awoken = new AtomicBoolean(false)
+  // Locks/condition variables are used so external events can fail
+  // us out. If we await the future and the future hangs, there's no
+  // reason to have the future in the first place. Only the future can
+  // "signal" itself. OTOH, anything can signal a condition. :)
+  private val _lock = new ReentrantLock()
+  private val _cond = _lock.newCondition()
 
-  def cmd_future = _cmd_future
+  override def run() = synchronized {
+    var watchers_snapshot = _watchers
+    var was_running = false
 
-  override def run_command() {
-    if (command_running) {
+    // Wow, this looks awkward. We need to reset watchers_snapshot
+    // and was_running based on what they are when we have the lock
+    // on watchers. This is because add_watcher does its interesting
+    // stuff when it has the same lock. Thus, if this is not done, 
+    // it's a race condition! And due to scoping, I can't just assign
+    // them inside of the synchronized block. If I'm just derping really
+    // hard and there's a better way to do this, *please* feel free to fix it.
+    synchronized {
+      watchers_snapshot = _watchers
+      was_running = _started.getAndSet(true)
+    }
+
+    if (!was_running) {
       val fut = Future {
-        super.run_command()
-        val res = command_result
-        notify_targ.map(_ ! (this, res))
-      } onComplete {
-        stop_self()
+        super.run()
+        result
+        // Don't call tell_targ here. If run() fails, then
+        // we still have to call tell_targ.
       }
-      _cmd_future = Some(fut)
+      fut.onFailure {
+        case _ =>
+          force_fail(even_if_success = false)
+          tell_targ(None)
+          wake_up()
+      }
+      fut.onSuccess {
+        case res =>
+          tell_targ(res)
+          wake_up()
+      }
+
+      // Fire off all monitors
+      for (w <- watchers_snapshot) {
+        w.start_monitoring()
+      }
     }
   }
 
-  def act() {
-    react {
-      case _ => 
-        signal_done()
-        exit()
+  def add_watcher(cmd_watcher: CommandWatcher, retroactive: Boolean = false) {
+    var started_after_add = false
+
+    synchronized {
+      started_after_add = _started.get()
+
+      if (!retroactive && started_after_add) {
+        throw new RuntimeException("Tried to add CommandWatcher when command already running.")
+      }
+
+      _watchers = cmd_watcher :: _watchers
+    }
+
+    // If we've already started the command, fire up the watcher...
+    // Done outside of synchronized block because this is (potentially) a 
+    // fairly lengthy operation.
+    if (started_after_add) {
+      cmd_watcher.start_monitoring()
     }
   }
 
-  def stop_self() { this ! this }
+  private def wake_up() {
+    val was_awoken = _awoken.getAndSet(true)
+    if (!was_awoken) {
+      _lock.lock()
+      _cond.signalAll()
+      _lock.unlock()
+    }
+  }
 
-  def command_running = _cmd_future != None
+  private def tell_targ(res: Option[T]) {
+    notify_targ map {
+      // Can't do _ ! (this, res) because it's interpreted as
+      // _.!(this, res), which is an invalid call to _.!.
+      val tup = (this, res)
+      _ ! tup
+    }
+  }
+
+  def command_running = _started.get() && !_awoken.get()
 
   def await(): Option[T] = {
-    command_success match {
-      // If success has not been reported, wait on the future.
-      case None => _cmd_future match {
-        case Some(x) => Await.result(x, max_wait)
-        case _ => None
+    // Attempt to run command on await(). Worst case, 
+    // we waste a few cycles. Commands are guaranteed not to be
+    // runnable multiple times per instance.
+    run()
+    try {
+      _lock.lock()
+      while (!_awoken.get()) {
+        _cond.await()
       }
-      case _ => command_result
+    } finally {
+      _lock.unlock()
     }
+    result
   }
 
-  def force_fail(autokill: Boolean, even_if_success: Boolean) {
-    // TODO:
+  def force_fail(even_if_success: Boolean, autokill: Boolean) {
     super.force_fail(even_if_success)
+    wake_up()
+    // TODO: NOTE: autokill is useless at the time of writing.
+    // There is no way to kill a future. Thus, tasks that never return just
+    // take up precious threads. We'll want to migrate to a thread model instead
+    // of a future model soon. But for now, Futures seem to work just fine.
   }
 
   override def force_fail(even_if_success: Boolean = false) {
@@ -118,7 +185,7 @@ trait Verifiable[T] {
   def command_failure = !command_success
 }
 
-/** 
+/**
  * CommandWatchers are entities that "watch" different parts of a command. They can
  * watch the execution time of a command, CPU graphs, whatever. What's important is
  * that, at any point, the command may be failed by the watcher.
@@ -126,18 +193,43 @@ trait Verifiable[T] {
 abstract class CommandWatcher(val watching: AndroidCommand[_]) {
   require(watching != null)
 
-  def command_monitor()
+  // Maybe we can name this better in the future ;)
+  protected var thread_monitor_for_me = true
+  private var _started = new AtomicBoolean(false)
+  private var _triggered = new AtomicBoolean(false)
 
-  def fail_command(force: Boolean = false) { 
+  // Override *this* method in order to give a monitoring implementation.
+  protected def do_monitor()
+
+  def started = _started.get()
+  def triggered = _triggered.get()
+
+  final def start_monitoring() {
+    _started.set(true)
+    if (thread_monitor_for_me) {
+      spawn {
+        do_monitor()
+      }
+    } else {
+      do_monitor()
+    }
+  }
+
+  protected def fail_command(force: Boolean = false) {
+    _triggered.set(true)
     watching.force_fail(force)
   }
 }
 
-class TimedCommandWatcher(watching: AsynchronousCommand[_], val time_limit: Duration) extends CommandWatcher(watching)
-{
+class TimedCommandWatcher(watching: AsynchronousCommand[_], val time_limit: FiniteDuration) extends CommandWatcher(watching) {
   require(time_limit != null)
+  thread_monitor_for_me = false
 
-  def command_monitor() {
-    val fut = Futures.alarm(time_limit.toMillis).onComplete { fail_command(force = false) }
+  val system = ActorSystem("system")
+
+  override def do_monitor() {
+    system.scheduler.scheduleOnce(time_limit) {
+      fail_command(force = false)
+    }
   }
 }
--- test/core/CommandWatcherTest.scala
+++ test/core/CommandWatcherTest.scala
@@ -1,6 +1,7 @@
 package core
 
 import org.scalatest.junit.AssertionsForJUnit._
+import org.scalatest.junit.AssertionsForJUnit
 import org.junit.Assert._
 import org.junit.Test
 import org.junit.Before
@@ -8,18 +9,19 @@ import org.junit.After
 import java.io.File
 import org.apache.commons.io.FileUtils
 import akka.testkit.TestKit
-import core.sdktools.CommandWatcher
+import scala.language.postfixOps
+import scala.concurrent.duration._
 
 class AndroidCommandTest extends AssertionsForJUnit  {
 
   /** Very basic test. Ensures absolute minimum functionality
     */
   @Test def testBasics {
-    val cmd = AndroidCommand[Integer] { 2 + 2 }
+    val cmd = new AndroidCommand[Integer](() => 2 + 2 )
     
-    assert(cmd.command_result == None)
-    cmd.run_command()
-    assert(cmd.command_result === 2)
+    assert(cmd.result == None)
+    cmd.run()
+    assert(cmd.result.getOrElse(-90909090) === 4)
   }
 
   /** This tests that the command is not executed at creation time.
@@ -27,21 +29,202 @@ class AndroidCommandTest extends AssertionsForJUnit  {
     */
   @Test def testTimeOfExecution {
     val time1 = System.currentTimeMillis()
-    val cmd = AndroidCommand[Unit] { Thread.sleep(1000) }
+    val cmd = new AndroidCommand[Unit](() => Thread.sleep(1000))
     val time2 = System.currentTimeMillis()
     assert(time2 - time1 < 1000)
-    cmd.run_command()
+    cmd.run()
     val time3 = System.currentTimeMillis()
     assert(time3 - time2 >= 1000)
-    assert(command_result != None)
+    assert(cmd.result != None)
   }
 }
 
-class AndroidCommandAsyncTest extends Actor with AssertionsForJUnit {
-  /** Once again, basic functionality
+class AndroidCommandAsyncTest extends AssertionsForJUnit {
+
+  /** Ensures absolute minimum functionality
     */
+  @Test def testBasics {
+    val cmd = new AsynchronousCommand[Integer](() => 2 + 2 )
+    
+    assert(cmd.result == None)
+    cmd.run()
+    assert(cmd.await() == Some(4))
+    assert(cmd.result == Some(4))
+  }
+
+  /** Guarantee that all commands are run async.
+    */
+  @Test def testAsyncrony {
+    val time1 = System.currentTimeMillis()
+    val cmd = new AsynchronousCommand[Unit](() => Thread.sleep(1000))
+    val time2 = System.currentTimeMillis()
+    assert(time2 - time1 < 1000)
+    assert(!cmd.command_running)
+    cmd.run()
+    assert(cmd.command_running)
+
+    val time3 = System.currentTimeMillis()
+    assert(time3 - time2 < 1000)
+    assert(cmd.result == None)
+
+    val res = cmd.await()
+    assert(!cmd.command_running)
+
+    val time4 = System.currentTimeMillis()
+    assert(time4 - time2 >= 1000)
+    assert(res != None)
+    assert(cmd.result != None)
+  }
+
+  /** Guarantee that multiple consecutive await()s will
+    * not fail.
+    */
+  @Test def multiAwait {
+    val cmd = new AsynchronousCommand[Unit](() => 2 + 2)
+    cmd.run()
+    val fst = cmd.await()
+    assert(fst != None)
+    assert(fst == cmd.result)
+    for(i <- 1 to 10) {
+      val a = cmd.await()
+      assert(a == fst)
+    }
+  }
+
+  /** Guarantee that force_fail wakes us up in a timely manner.
+    */
+  @Test def wakeUp {
+    val cmd = new AsynchronousCommand[Unit](() => Thread.sleep(1000))
+    val time1 = System.currentTimeMillis()
+    cmd.run()
+    cmd.force_fail(even_if_success = true)
+    // Guarantee that force_fail doesn't do the waiting for us...
+    val time2 = System.currentTimeMillis()
+    assert(time2 - time1 < 1000)
+    cmd.await()
+    val time3 = System.currentTimeMillis()
+    // Less than 1000ms have passed since start? Cool!
+    assert(time3 - time1 < 1000)
+    assert(cmd.success == Some(false))
+    assert(cmd.result == None)
+  }
+
+  @Test def forceFail {
+    val cmd = new AsynchronousCommand[Integer](() => 2 + 2)
+    assert(cmd.await() == Some(4))
+    assert(cmd.success == Some(true))
+    assert(cmd.result == Some(4))
+
+    cmd.force_fail(even_if_success = false)
+    assert(cmd.await() == Some(4))
+    assert(cmd.success == Some(true))
+    assert(cmd.result == Some(4))
+
+    cmd.force_fail(even_if_success = true)
+    assert(cmd.await() == None)
+    assert(cmd.success == Some(false))
+    assert(cmd.result == None)
+  }
+
+  class CommandWatcher_watchers(cmd: AndroidCommand[_]) extends CommandWatcher(cmd) {
+    require(cmd != null)
+    thread_monitor_for_me = false
+
+    override def do_monitor() {}
+  }
+
+  @Test def watchers {
+    val cmd = new AsynchronousCommand(() => 2 + 2)
+    val watcher = new CommandWatcher_watchers(cmd)
+    cmd.add_watcher(watcher)
+    assert(!watcher.started)
+    assert(!watcher.triggered)
+    cmd.run()
+    assert(cmd.success != None)
+    assert(watcher.started)
+    assert(!watcher.triggered)
+  }
 }
 
-class CommandWatcherTest extends AssertionsForJUnit {
-  // TODO --> 
+class TimedCommandWatcherTest extends AssertionsForJUnit {
+
+  /** Test fail (i.e. CommandWatcher must "fail" the result.)
+    */
+  @Test def testFailResult {
+    val cmd = new AsynchronousCommand(() => Thread.sleep(1000))
+    val watch = new TimedCommandWatcher(cmd, 100 millis)
+
+    cmd.add_watcher(watch)
+    assert(!watch.started)
+    assert(!watch.triggered)
+
+    val time1 = System.currentTimeMillis()
+    cmd.run()
+    assert(watch.started)
+    assert(!watch.triggered)
+
+    cmd.await()
+    val time2 = System.currentTimeMillis()
+    assert(time2 - time1 < 1000)
+    assert(watch.started)
+    assert(watch.triggered)
+    assert(cmd.result == None)
+    assert(cmd.success == Some(false))
+  }
+
+  // Test pass (i.e. CommandWatcher must not "fail" the result.)
+  @Test def testPassResult {
+    val cmd = new AsynchronousCommand(() => Thread.sleep(100))
+    val watch = new TimedCommandWatcher(cmd, 1000 millis)
+
+    cmd.add_watcher(watch)
+    assert(!watch.started)
+    assert(!watch.triggered)
+
+    val time1 = System.currentTimeMillis()
+    cmd.run()
+    assert(watch.started)
+    assert(!watch.triggered)
+
+    cmd.await()
+    val time2 = System.currentTimeMillis()
+    assert(time2 - time1 < 1000)
+    assert(watch.started)
+    assert(!watch.triggered)
+    assert(cmd.success == Some(true))    
+    assert(cmd.result != None)
+  }
+
+  @Test def testPassResultFull {
+    val cmd = new AsynchronousCommand(() => Thread.sleep(100))
+    val watch = new TimedCommandWatcher(cmd, 500 millis)
+
+    cmd.add_watcher(watch)
+    assert(!watch.started)
+    assert(!watch.triggered)
+
+    val time1 = System.currentTimeMillis()
+    cmd.run()
+    assert(watch.started)
+    assert(!watch.triggered)
+
+    cmd.await()
+    val time2 = System.currentTimeMillis()
+    assert(time2 - time1 >= 100 && time2 - time1 < 500)
+    assert(!watch.triggered)
+    assert(cmd.success == Some(true))
+    val oldres = cmd.result
+    assert(cmd.result != None)
+
+    // Give the watcher 750ms to finish, because why not?
+    Thread.sleep(750)
+    assert(watch.started)
+    assert(watch.triggered)
+
+    // Cool. Now that it's been finished and triggered, make sure
+    // that nothing was reset by it.
+    assert(cmd.success == Some(true))
+    assert(cmd.result != None)
+    assert(cmd.result == oldres)
+  }
 }
