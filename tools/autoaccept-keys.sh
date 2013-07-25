a119aae3c8dd1d490b5ec58222c9669f2d317020
Brandon Amos <bdamos@vt.edu>
2013-07-24 20:52:38 -0700

    Fix autoaccept-keys.

--- tools/autoaccept-keys.sh
+++ tools/autoaccept-keys.sh
@@ -2,7 +2,7 @@
 #
 # Automatically accept keys from all nodes.
 
-# http://askubuntu.com/questions/123072
 for ((i=1; i<=34; i++)); do
-    yes yes | ssh "10.0.2.$i" "uptime";
+    ssh-keyscan -t rsa "10.0.2.$i" >> ~/.ssh/known_hosts
+    ssh-keyscan -t rsa "b$i" >> ~/.ssh/known_hosts
 done
