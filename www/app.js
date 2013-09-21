565fef28d39989372bfa135d9c66b0b8dd221c8d
Brandon Amos <bdamos@vt.edu>
2013-09-21 04:40:59 -0400

    Clasp: Add documentation.

--- www/app.js
+++ www/app.js
@@ -16,7 +16,6 @@ app.configure(function(){
   app.set('port', process.env.PORT || 9090);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
@@ -31,6 +30,7 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
+app.get('/doc', routes.doc);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- www/routes/index.js
+++ www/routes/index.js
@@ -1,3 +1,7 @@
 exports.index = function(req, res){
   res.render('index');
 };
+
+exports.doc = function(req, res) {
+  res.render('doc');
+}
