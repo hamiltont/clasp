dddcf52737eb24f6cb8f36f57fab8ef598126c80
Brandon Amos <bdamos@vt.edu>
2014-02-02 19:48:58 -0500

    Add 20 random contacts for #42.

--- android-config/src/main/scala/MainActivity.scala
+++ android-config/src/main/scala/MainActivity.scala
@@ -2,12 +2,49 @@ package clasp.android_config
 
 import _root_.android.app.Activity
 import _root_.android.os.Bundle
+import _root_.android.content.ContentProviderOperation
+import _root_.android.provider.ContactsContract
+import _root_.android.provider.ContactsContract.{Data,RawContacts}
+import _root_.android.provider.ContactsContract.CommonDataKinds.StructuredName
+
+import java.util.ArrayList
 
 class MainActivity extends Activity with TypedActivity {
+  // Generated with http://listofrandomnames.com
+  val names = List(
+    "Rod Basham", "Alvaro Reifsteck", "Ginette Vester", "Eliseo Pollock",
+    "Lashon Edgemon", "Jeanette Rutledge", "Mac Mccourt", "Grover Freese",
+    "Leeann Harney", "Dulce Ahner", "Raleigh Clampitt", "Bari Dinsmore",
+    "Alejandra Scheffler", "Annamarie Mahle", "Hee Patnaude",
+    "Crista Hernandes", "Marcela Welter", "Kaitlyn Zambrana",
+    "Yolanda Woosley", "Bebe Leveille", "Trinity Hedstrom", "Mitzi Metayer",
+    "Petra Clingman", "Johnetta Mcmeen", "Monnie Pantoja", "Garret Dedrick",
+    "Akilah Linn", "Eilene Paquet", "Jasmin Heisler", "Sina Blansett"
+  )
+
   override def onCreate(bundle: Bundle) {
     super.onCreate(bundle)
     setContentView(R.layout.main)
 
-    findView(TR.textview).setText("hello, world!")
+
+    val ops = new ArrayList[ContentProviderOperation]()
+    // https://github.com/android/platform_frameworks_base/blob/master/core/java/android/provider/ContactsContract.java
+    // Not sure why this can't access RawContacts.ACCOUNT_TYPE or
+    // RawContacts.ACCOUNT_NAME, but hard coding works for now.
+    names map { name:String =>
+      val rawContactInsertIndex = ops.size
+      ops.add(ContentProviderOperation.newInsert(RawContacts.CONTENT_URI)
+        .withValue("account_type", "person")
+        .withValue("account_name", name)
+        .build
+      )
+      ops.add(ContentProviderOperation.newInsert(Data.CONTENT_URI)
+        .withValueBackReference("raw_contact_id", rawContactInsertIndex)
+        .withValue("mimetype", StructuredName.CONTENT_ITEM_TYPE)
+        .withValue(StructuredName.DISPLAY_NAME, name)
+        .build
+      )
+    }
+    getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops)
   }
 }
