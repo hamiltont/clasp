553c80cce0a7e324de4edb46e07c1f6bee7df835
Hamilton Turner <hamiltont@gmail.com>
2014-09-29 11:36:00 -0400

    Properly ID canvas and log div. Rewrite dashboard JS

--- www/public/scripts/dashboard.js
+++ www/public/scripts/dashboard.js
@@ -1,23 +1,32 @@
 // dashboard.js
-// Provides a client-side interface to obtain the status
-// and control servers for Clasp.
+// 
+// Sets up core dashboard functions, timers, connections, etc
 //
 // Brandon Amos
 // 2013.09.22
+//
+// Hamilton Turner
+// 2014.08 - Removed all functions and rewrote file
 
-var root = 'localhost:8080';
-var connection = new WebSocket('ws://' + root);
+clasp.root = 'localhost:8080';
+clasp._websocket = new WebSocket('ws://' + clasp.root);
+clasp.pubsub = new WebSocketMultiplex(clasp._websocket); 
 
-var servers = []
-var emulators = []
+// Example pubsub usage: 
+//   var foo = clasp.pubsub.channel('foo');
+//   foo.send('barf');
+//   // TODO ensure the syntax of these two is correct
+//   elogs.addEventListener('message', function(dataObject) {
+//     console.log('elogs got data ' + dataObject.data);
+//   });
+//   foo.onclose(function() {});
+//   foo.close();
 
-
-function shutdown() {
-    $.ajax('http://' + root + '/system/shutdown')
-    .done(function( data ) {
-        console.log('Shutting down');
-    });
-}
+//var elogs = clasp.pubsub.channel('/emulator/' + uuid + '/log');
+//elogs.addEventListener('message', function(dataObject) {
+//  console.log('elogs got data ' + dataObject.data);
+//  $('#emulatorlogs').append(dataObject.data + '<br />');
+//});
 
 $.ajax('http://' + root + '/nodes/all')
   .done(function( data ) {
@@ -33,6 +42,9 @@ $.ajax('http://' + root + '/nodes/all')
     console.log( "Server request failed: " + data );
   });
 
+// TODO only refresh's emulators currently
+clasp.refresh = function () {
+    $.ajax('http://' + clasp.root + '/emulators')
       .done(function( data ) {
         redraw_emulator_list(data);
       }).fail(function( jqXHR, textStatus, errorThrown ) {
@@ -40,17 +52,26 @@ $.ajax('http://' + root + '/nodes/all')
       });    
 }
 
+// Setup some logging on the raw websocket
+// Other consumers should use the pub-sub interface
+// provided in clasp.pubsub
+clasp._websocket.onopen = function () {
+  clasp._websocket.onclose = function(close) {
+    console.log('WebSocket: Connection closed');
   };
-
-  connection.onerror = function (error) {
-    console.log('WebSocket Error ' + error);
+  clasp._websocket.onerror = function (error) {
+    console.log('WebSocket: Error ' + error);
   };
-
-  connection.onmessage = function (e) {
-    console.log('Server: ' + e.data);
+  clasp._websocket.onmessage = function (e) {
+    console.log('WebSocket: Server Message: ' + e.data);
   };
 };
 
+clasp.shutdown = function() {
+    $.ajax('http://' + clasp.root + '/system/shutdown')
+    .done(function( data ) {
+        console.log('Shutting down');
+    });
 }
 
 
