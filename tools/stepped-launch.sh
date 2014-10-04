253c57a9181dec3c86958acda6cda222309a83a7
Hamilton Turner <hamiltont@gmail.com>
2014-10-04 01:55:14 -0400

    Minor script improvements

--- tools/stepped-launch.sh
+++ tools/stepped-launch.sh
@@ -3,9 +3,9 @@
 # and then this script will launch a new emulator every 8
 # minutes until it's launched 10
 
-REST_API="http://10.0.2.17:8080"
+REST_API=$1
 
-for ((i=1; i<=20; i++)); do
+for ((i=1; i<=80; i++)); do
     echo "Launching $i"
     echo "Using curl $REST_API/emulators/launch"
     curl "$REST_API/emulators/launch"
@@ -20,5 +20,5 @@ for ((i=1; i<=20; i++)); do
     echo "Awake, checking if emulators have launched..."
     echo "Using curl $REST_API/emulators"
     curl "$REST_API/emulators"
-    curl "$REST_API/emulators" > "$i-emulators.out"
+    curl "$REST_API/emulators" > "$i-emulators-$REST_API.txt"
 done
